60666bd80a057a7dc5e33c4dd0bf3926aa545e08
Yasmin-brito <yasmincarneiro2005@gmail.com>
2025-12-03 16:12:28 -0300

    Criando classe Recorde para salvar nome + pontuação para ser usado no historico

---
+++ Recorde.java
@@ -0,0 +1,17 @@
+public class Recorde {
+    private String nome;
+    private int pontos;
+
+    public Recorde(String nome, int pontos){
+        this.nome = nome;
+        this.pontos = pontos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    
+    public int getPontos() {
+        return pontos;
+    }
+}
